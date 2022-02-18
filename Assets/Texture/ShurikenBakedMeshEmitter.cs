4cd32c0bcfa57fb9869b99b878f8035aa802faff
Batuhan Eyüboğlu <batuhan.eyuboglu@outlook.com>
2022-02-19 00:24:06 +0300

    Paket

---
+++ Assets/Texture/ShurikenBakedMeshEmitter.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShurikenBakedMeshEmitter : MonoBehaviour {
+	public SkinnedMeshRenderer skin;
+	Mesh baked;
+	ParticleSystem particle;
+	ParticleSystemRenderer render;
+	public bool emit;
+	public float coolDown = 0.5f;
+	float interval = 0;
+
+	void Start () {
+		if(!skin)
+			this.enabled = false;
+
+		particle = GetComponent<ParticleSystem>();
+		render = GetComponent<ParticleSystemRenderer>();
+	}
+	
+	void Update () {
+		if(emit){			
+			interval -= Time.deltaTime;
+			if(interval < 0){
+				GameObject newEmitter = Instantiate(gameObject, transform.position, transform.rotation) as GameObject;
+				newEmitter.GetComponent<ShurikenBakedMeshEmitter>().EmitMesh();
+				interval = coolDown;
+			}
+		}else{
+			interval = coolDown;
+		}
+	}
+
+	public void EmitMesh () {
+		emit = false;
+		baked = new Mesh();
+		skin.BakeMesh(baked);
+		particle = GetComponent<ParticleSystem>();
+		render = GetComponent<ParticleSystemRenderer>();
+		render.mesh = baked;
+		particle.Play();
+		Destroy(gameObject, particle.duration);
+	}
+}
